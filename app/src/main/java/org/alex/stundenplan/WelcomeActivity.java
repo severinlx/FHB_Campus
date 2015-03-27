fc65e7ef9115690874c3bd6243775c5a76381d70
André <andre.nitze@fh-brandenburg.de>
2015-03-27 11:27:53 +0100

    Correct typo

--- app/src/main/java/org/alex/stundenplan/WelcomeActivity.java
+++ app/src/main/java/org/alex/stundenplan/WelcomeActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import org.alex.stundenplan.stundenplan.PlanActivity;
 
@@ -22,7 +21,7 @@ import org.alex.stundenplan.stundenplan.PlanActivity;
  *  3 design
  * 
  */
-public class WellcomeActivity extends Activity{
+public class WelcomeActivity extends Activity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
